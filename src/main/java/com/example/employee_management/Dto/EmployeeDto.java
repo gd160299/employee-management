ac2303e3a67cad644b9489b23604f6a801af789e
giangdh <giangdh@etc.vn>
2024-03-07 09:21:58 +0700

    add totalElements for API search

--- src/main/java/com/example/employee_management/Dto/EmployeeDto.java
+++ src/main/java/com/example/employee_management/Dto/EmployeeDto.java
@@ -4,37 +4,39 @@ import com.example.employee_management.Common.DbColumnMapper;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 @Data
 public class EmployeeDto {
-    @DbColumnMapper("Employee_ID")
-    private Long employeeId;
+	@DbColumnMapper("Employee_ID")
+	private Long employeeId;
 
-    @DbColumnMapper("Employee_Name")
-    private String employeeName;
+	@DbColumnMapper("Employee_Name")
+	private String employeeName;
 
-    @DbColumnMapper("Department_ID")
-    private Long departmentId;
+	@DbColumnMapper("Department_ID")
+	private Long departmentId;
 
-    @DbColumnMapper("Salary")
-    private BigDecimal salary;
+	@DbColumnMapper("Salary")
+	private BigDecimal salary;
 
-    @DbColumnMapper("User_name")
-    private String userName;
+	@DbColumnMapper("User_name")
+	private String userName;
 
-    @DbColumnMapper("PHONE_NUMBER")
-    private String phoneNumber;
+	@DbColumnMapper("PHONE_NUMBER")
+	private String phoneNumber;
 
-    @DbColumnMapper("Position")
-    private String position;
+	@DbColumnMapper("Position")
+	private String position;
 
-    @DbColumnMapper("EMAIL")
-    private String email;
+	@DbColumnMapper("EMAIL")
+	private String email;
 
-    @DbColumnMapper("PASS_WORD")
-    private String passWord;
+	@DbColumnMapper("PASS_WORD")
+	private String passWord;
 
-    @DbColumnMapper("department_name")
-    private String departmentName;
+	@DbColumnMapper("department_name")
+	private String departmentName;
+
+	@DbColumnMapper("TOTAL_ELEMENTS")
+	private Long totalElements;
 }
